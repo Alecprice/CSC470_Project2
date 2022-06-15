1207c6bc28b37c216b97abc5d737905eb2499061
Alecprice <52548136+Alecprice@users.noreply.github.com>
2022-06-14 23:50:57 -0400

    Add files via upload

---
+++ Prj2/Employee-Pay_Calculator/Employee-Pay_Calculator/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Employee_Pay_Calculator
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ Prj2/Employee-Pay_Calculator/Employee-Pay_Calculator/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Pay_Calculator
+{
+	class Employee
+	{
+		private String firstName;
+		private String lastName;
+		private int age;
+		private double hourlyRate;
+		private double OT = 1.5;
+		private double weeklyPay;
+		private double defaultWorkWeek = 40;
+		private double hoursWorked;
+		private double formula1;
+		private double formula2;
+
+		public Employee()
+		{
+			Console.WriteLine("This is the default constructor");
+		}
+
+		public void employeeValues(String firstName, String lastName, int age, double hourlyRate, double hoursWorked)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.age = age;
+			this.hourlyRate = hourlyRate;
+			this.hoursWorked = hoursWorked;
+			
+			
+
+		}
+
+		public double calc()
+		{
+			formula1 = (hoursWorked - defaultWorkWeek) * (hourlyRate * OT);
+			formula2 = (defaultWorkWeek * hourlyRate);
+
+			return formula1 + formula2;
+		}
+
+	}
+}
